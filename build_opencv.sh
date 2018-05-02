a48fd9e9a20c926f64390061144cf75a52d39dd6
exipilis <malenko.andrii@gmail.com>
2018-05-02 20:56:55 +0300

    gitignore

---
+++ build_opencv.sh
@@ -0,0 +1,2 @@
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local PYTHON3_EXECUTABLE=/usr/bin/python3.5 OPENCV_EXTRA_MODULES_PATH=/home/malena/PycharmProjects/plate_detector/opencv_contrib/modules ..
+
